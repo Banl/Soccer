f39f5b4b01d87d9adf33130cf4e504fc7e646a53
Jean <jpoolvalverde@gmail.com>
2020-03-26 19:51:32 -0400

    Add migrations and Teams comtrolere

---
+++ Soccer.Web/Migrations/20200326234503_AddTeamEntity.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Soccer.Web.Migrations
+{
+    public partial class AddTeamEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 50, nullable: false),
+                    LogoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TournamentEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false),
+                    IsActive = table.Column<bool>(nullable: false),
+                    LogoPath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TournamentEntity", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserEntity",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    UserName = table.Column<string>(nullable: true),
+                    NormalizedUserName = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    NormalizedEmail = table.Column<string>(nullable: true),
+                    EmailConfirmed = table.Column<bool>(nullable: false),
+                    PasswordHash = table.Column<string>(nullable: true),
+                    SecurityStamp = table.Column<string>(nullable: true),
+                    ConcurrencyStamp = table.Column<string>(nullable: true),
+                    PhoneNumber = table.Column<string>(nullable: true),
+                    PhoneNumberConfirmed = table.Column<bool>(nullable: false),
+                    TwoFactorEnabled = table.Column<bool>(nullable: false),
+                    LockoutEnd = table.Column<DateTimeOffset>(nullable: true),
+                    LockoutEnabled = table.Column<bool>(nullable: false),
+                    AccessFailedCount = table.Column<int>(nullable: false),
+                    Document = table.Column<string>(maxLength: 20, nullable: false),
+                    FirstName = table.Column<string>(maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(maxLength: 50, nullable: false),
+                    Address = table.Column<string>(maxLength: 500, nullable: true),
+                    PicturePath = table.Column<string>(nullable: true),
+                    UserType = table.Column<int>(nullable: false),
+                    TeamId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_UserEntity_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(maxLength: 30, nullable: false),
+                    TournamentId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupEntity_TournamentEntity_TournamentId",
+                        column: x => x.TournamentId,
+                        principalTable: "TournamentEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GroupDetailEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    TeamId = table.Column<int>(nullable: true),
+                    MatchesPlayed = table.Column<int>(nullable: false),
+                    MatchesWon = table.Column<int>(nullable: false),
+                    MatchesTied = table.Column<int>(nullable: false),
+                    MatchesLost = table.Column<int>(nullable: false),
+                    GoalsFor = table.Column<int>(nullable: false),
+                    GoalsAgainst = table.Column<int>(nullable: false),
+                    GroupId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GroupDetailEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_GroupDetailEntity_GroupEntity_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "GroupEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_GroupDetailEntity_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "MatchEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false),
+                    LocalId = table.Column<int>(nullable: true),
+                    VisitorId = table.Column<int>(nullable: true),
+                    GoalsLocal = table.Column<int>(nullable: true),
+                    GoalsVisitor = table.Column<int>(nullable: true),
+                    IsClosed = table.Column<bool>(nullable: false),
+                    GroupId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MatchEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MatchEntity_GroupEntity_GroupId",
+                        column: x => x.GroupId,
+                        principalTable: "GroupEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MatchEntity_Teams_LocalId",
+                        column: x => x.LocalId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_MatchEntity_Teams_VisitorId",
+                        column: x => x.VisitorId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PredictionEntity",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    MatchId = table.Column<int>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    GoalsLocal = table.Column<int>(nullable: true),
+                    GoalsVisitor = table.Column<int>(nullable: true),
+                    Points = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PredictionEntity", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_PredictionEntity_MatchEntity_MatchId",
+                        column: x => x.MatchId,
+                        principalTable: "MatchEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_PredictionEntity_UserEntity_UserId",
+                        column: x => x.UserId,
+                        principalTable: "UserEntity",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupDetailEntity_GroupId",
+                table: "GroupDetailEntity",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupDetailEntity_TeamId",
+                table: "GroupDetailEntity",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GroupEntity_TournamentId",
+                table: "GroupEntity",
+                column: "TournamentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchEntity_GroupId",
+                table: "MatchEntity",
+                column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchEntity_LocalId",
+                table: "MatchEntity",
+                column: "LocalId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MatchEntity_VisitorId",
+                table: "MatchEntity",
+                column: "VisitorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PredictionEntity_MatchId",
+                table: "PredictionEntity",
+                column: "MatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PredictionEntity_UserId",
+                table: "PredictionEntity",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserEntity_TeamId",
+                table: "UserEntity",
+                column: "TeamId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GroupDetailEntity");
+
+            migrationBuilder.DropTable(
+                name: "PredictionEntity");
+
+            migrationBuilder.DropTable(
+                name: "MatchEntity");
+
+            migrationBuilder.DropTable(
+                name: "UserEntity");
+
+            migrationBuilder.DropTable(
+                name: "GroupEntity");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "TournamentEntity");
+        }
+    }
+}
